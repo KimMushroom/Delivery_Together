488f79e703bf229f12262689b84513c0a21e2e66
cholong2 <pcl106@naver.com>
2021-05-01 16:41:43 +0900

    Revert "test4"
    This reverts commit 354ad5d74ff7a5bb09f24bc472120c172c51b69f.

--- app/src/main/java/com/mpproject/delivery_together/MainActivity.java
+++ app/src/main/java/com/mpproject/delivery_together/MainActivity.java
@@ -3,20 +3,14 @@ package com.mpproject.delivery_together;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import androidx.annotation.IdRes;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,86 +24,181 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Fragment1 fragment1;
-    Fragment2 fragment2;
-    Fragment3 fragment3;
-    BottomNavigationView bottomNavigation;
+    private String lifeCycleTag = "Main Activity";
+    private String tag = "MainActivity Message";
 
+    private long backKeyPressedTime = 0;
+
+    private RecyclerAdapter adapter;
+    private Button writeBtn;
+
+    private List<String> titleList = new ArrayList<String>();  //게시물 제목
+    private List<String> contentList = new ArrayList<String>();  //게시물 내용
+    private List<String> keyList = new ArrayList<String>();  //게시물 키값
+    private List<String> uidList = new ArrayList<>();  //게시물 작성자 id
+
+    private DatabaseReference database;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(lifeCycleTag, "In the onCreate() event");  // life cycle 확인용
 
+        //화면 초기화
+        //getData();
 
+        database = FirebaseDatabase.getInstance().getReference("post");
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        Fragment1 fragment1;
-        Fragment2 fragment2;
-        Fragment3 fragment3;
+        database.addValueEventListener(new ValueEventListener() {
 
-
-
-        //oncreate()안에 각각 객체로 만들어 변수에 할당
-        fragment1 = new Fragment1();
-
-        fragment2 = new Fragment2();
-
-        fragment3 = new Fragment3();
-
-        //FragmentMAnager객체--> 첫번째 프레임 레이아웃 안에 추가되도록 한다.
-        getSupportFragmentManager().
-
-                beginTransaction().
-
-                replace(R.id.container, fragment1).
-
-                commit();
-
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
-
-        {
-            //하단탭에 들어있는 각각의 버튼 누를 시, ㅁ()메서드가 자동호출 -> 메뉴 아이템의 id값으로 버튼구분, 토스트띄움
+            //게시물 데이터베이스 값 변동시 발생
             @Override
-            public boolean onNavigationItemSelected (@NonNull MenuItem item){
-                switch (item.getItemId()) {
-                    case R.id.tab1:
-                        Toast.makeText(getApplicationContext(), "home 메인 화면", Toast.LENGTH_LONG).show();
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, fragment1).commit();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //리사이클 뷰 초기화
+                titleList.clear();
+                contentList.clear();
+                keyList.clear();
+                uidList.clear();
 
-                        return true;
-                    case R.id.tab2:
-                        Toast.makeText(getApplicationContext(), "나의 채팅방", Toast.LENGTH_LONG).show();
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, fragment2).commit();
+                //****현재 조건에 맞게 게시물 보여주기****
 
-                        return true;
-                    case R.id.tab3:
-                        Toast.makeText(getApplicationContext(), "마이 페이지", Toast.LENGTH_LONG).show();
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, fragment3).commit();
-
-                        return true;
+                //database있을때 까지 리사이클 뷰 채우기
+                for (DataSnapshot message : dataSnapshot.getChildren()) {
+                    String str1 = (String) message.child("title").getValue();
+                    String str2 = (String) message.child("content").getValue();
+                    String str3 = (String) message.child("uid").getValue();
+                    String key = message.getKey();
+                    keyList.add(key);
+                    titleList.add(str1);
+                    contentList.add(str2);
+                    uidList.add(str3);
                 }
+                //화면에 출력
+                getData();
+            }
 
-                return false;
+            public void onCancelled(DatabaseError databaseError) {
             }
         });
 
+        //글쓰기 버튼 누르고 화면전환
+        //버튼 누르면 write 화면
+        writeBtn = (Button) findViewById(R.id.writeBtn);
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(tag, "Write Button Clicked");  //게시물 작성 버튼 클릭됨
+
+                Intent intent = new Intent(getApplicationContext(), PostWriteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(lifeCycleTag, "In the onStart() event");  // life cycle 확인용
+    }
 
-    //하나의 프래그먼트에서 다른 프래그먼트로 전환하는 용도로 정의한 인터페이스
-    public void onTabSelected(int position) {
-        if (position == 0) {
-            bottomNavigation.setSelectedItemId(R.id.tab1);
-        } else if (position == 1) {
-            bottomNavigation.setSelectedItemId(R.id.tab2);
-        } else if (position == 2) {
-            bottomNavigation.setSelectedItemId(R.id.tab3);
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(lifeCycleTag, "In the onRestart() event");  // life cycle 확인용
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(lifeCycleTag, "In the onResume() event");  // life cycle 확인용
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(lifeCycleTag, "In the onPause() event");  // life cycle 확인용
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(lifeCycleTag, "In the onStop() event");  // life cycle 확인용
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(lifeCycleTag, "In the onDestroy() event");  // life cycle 확인용
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        // 기존 뒤로 가기 버튼의 기능을 막기 위해 주석 처리 또는 삭제
+
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지났으면 Toast 출력
+        // 2500 milliseconds = 2.5 seconds
+        if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+            backKeyPressedTime = System.currentTimeMillis();
+            Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지나지 않았으면 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+            firebaseAuth.signOut();
+            finish();
         }
     }
+
+    private void getData() {
+        init();
+
+        for (int i = 0; i < titleList.size(); i++) {
+            RecyclerItem data = new RecyclerItem();
+            data.setTitle(titleList.get(i));
+            data.setCont(contentList.get(i));
+
+            adapter.addItem(data);
+        }
+        Log.d(tag, "Post Reload");  //게시물 리스트 보여주기
+    }
+
+    private void init() {
+        //초기화 함수
+        //리사이클뷰 새로 만들어주기
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+
+        adapter = new RecyclerAdapter();
+        //리사이클뷰 클릭이벤트
+        //특정 리사이클뷰 클릭시 그 글 post에 들어가고 title content key값 넘김
+        adapter.setOnItemClickListener(new RecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int pos) {
+                //****추가적인 정보 더 보내기
+                Intent intent = new Intent(getApplicationContext(), PostInfoActivity.class);
+                Bundle bundle=new Bundle();
+
+                bundle.putString("title", titleList.get(pos));
+                bundle.putString("content", contentList.get(pos));
+                bundle.putString("key", keyList.get(pos));
+                bundle.putString("uid", uidList.get(pos));
+
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+    }
+
 
 }
 
--- app/src/main/java/com/mpproject/delivery_together/OnTabItemSelectedListener.java
+++
@@ -1,7 +0,0 @@
-package com.mpproject.delivery_together;
-
-
-//mainactivity클래스에 구현되어있는 인터페이스
-public interface OnTabItemSelectedListener {
-    public void onTabSelected(int position);
-}
